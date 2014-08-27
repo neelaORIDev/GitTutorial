d8c7ba44b11c6ad7326561929e1bc2b00d98bb05
neelaORIDev <sneelakandan@optiriskindia.com>
2014-08-27 10:35:47 +0530

    Added Test Case Comment

--- src/com/ori/git/tutorial/Test.java
+++ src/com/ori/git/tutorial/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// My Test Case
         for(int i = 0; i < 10; i++){
         	System.out.println("I: "+i);
         }
